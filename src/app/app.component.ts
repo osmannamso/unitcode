225339b35fa21ad85cff053ab372907486a3c6f4
Osman <osmannamso123@gmail.com>
2020-11-24 14:00:53 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GAME_CHECK_LINES} from '../values/variables';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'unitcode';
+  currentTurn: 'x' | 'o' = 'x';
+  desk = [['', '', ''], ['', '', ''], ['', '', '']];
+  gameCheckLines = GAME_CHECK_LINES;
+
+  setMark(x, y): void {
+    if (!this.desk[x][y]) {
+      this.desk[x][y] = this.currentTurn;
+      this.currentTurn = this.currentTurn === 'x' ? 'o' : 'x';
+      if (this.gameCheck()) {
+        this.clearDesk();
+      }
+    }
+  }
+
+  clearDesk(): void {
+    this.currentTurn = 'x';
+    this.desk = [['', '', ''], ['', '', ''], ['', '', '']];
+  }
+
+  gameCheck(): boolean {
+    let finish = false;
+
+    this.gameCheckLines.forEach((probables) => {
+      let crossed = true;
+      const startedMark = this.desk[probables[0][0]][probables[0][1]];
+
+      if (startedMark && !finish) {
+        probables.forEach((item) => {
+          if (!this.desk[item[0]][item[1]] || this.desk[item[0]][item[1]] !== startedMark) {
+            crossed = false;
+          }
+        });
+
+        finish = crossed;
+      }
+    });
+
+    return finish;
+  }
 }
